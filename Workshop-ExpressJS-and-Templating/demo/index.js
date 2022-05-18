9c5fc3de998f97fbc16ebe396300d4d1f5a7dc08
DannyDoneva96 <danielasinmas1@gmail.com>
2022-05-18 11:55:45 +0300

    post req

--- Workshop-ExpressJS-and-Templating/demo/index.js
+++ Workshop-ExpressJS-and-Templating/demo/index.js
@@ -2,7 +2,13 @@ let express = require('express');
 
 
 let app = express()
-app.get('/', function(req, res) {res.send('Welcome')})
+app.get('/', function (req, res) { res.send('Welcome') })
 
+
+
+app.post('/create', function (req, res) {
+    res.status(201).send('created')
+
+})
 
 app.listen(3000)
