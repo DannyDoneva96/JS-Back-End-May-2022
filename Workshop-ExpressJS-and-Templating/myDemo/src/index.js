2d4689e88df29dcf9fad4c7cbacbf1cfefa8f01f
DannyDoneva96 <danielasinmas1@gmail.com>
2022-06-08 11:27:18 +0300

    demo

--- Workshop-ExpressJS-and-Templating/myDemo/src/index.js
+++ Workshop-ExpressJS-and-Templating/myDemo/src/index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
+const handlebars = require('express-handlebars')
+app.use(express.static('public'));
+
+app.engine('hbs',handlebars.engine({
+    extname:'hbs',
+
+}));
+app.set('view engine','hbs')
+
 const port = 3000;
 
 app.get('/', (req, res) =>{
-    res.status(200).send('Welcome!')
+    res.render('index.hbs',)
 })
 
 
