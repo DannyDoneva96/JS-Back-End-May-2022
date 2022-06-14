a67f451f90b75556992d071a2ce890b9ec9c74ce
DannyDoneva96 <danielasinmas1@gmail.com>
2022-06-14 09:12:01 +0300

    initialize and config express app

---
+++ Workshop-ExpressJS-and-Templating/Demo-V.K/demo/index.js
@@ -0,0 +1,16 @@
+// init express application
+//initialize templating lib 
+//create home controller
+//bind routing
+//create data source
+//implement controllers
+
+const express = require('express');
+const hbs = require('express-handlebars');
+const app = express();
+app.engine("hbs",hbs.create({extname: '.hbs'}).engine);
+app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({ extended:true }));
+app.use('/static',express.static('static'));
+app.listen(3000, function(){console.log('listening on port 3000');});
