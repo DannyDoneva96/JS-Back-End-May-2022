58cdf4057479c569121662a90cf46fda7240748f
DannyDoneva96 <danielasinmas1@gmail.com>
2022-06-14 09:35:53 +0300

    controllers

--- Workshop-ExpressJS-and-Templating/Demo-V.K/demo/index.js
+++ Workshop-ExpressJS-and-Templating/Demo-V.K/demo/index.js
@@ -4,15 +4,24 @@
 //bind routing
 //create data source
 //implement controllers
-const {home} = require('./controllers/home')
+const { home } = require('./controllers/home')
 
 const express = require('express');
 const hbs = require('express-handlebars');
+const { about } = require('./controllers/about');
+const { create } = require('./controllers/create');
+const { details } = require('./controllers/details');
+const {notFound} = require('./controllers/notFound');
 const app = express();
-app.engine("hbs",hbs.create({extname: '.hbs'}).engine);
+app.engine("hbs", hbs.create({ extname: '.hbs' }).engine);
 app.set('view engine', 'hbs');
 
-app.use(express.urlencoded({ extended:true }));
-app.use('/static',express.static('static'));
-app.get('/',home)
-app.listen(3000, function(){console.log('listening on port 3000');});
+app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static('static'));
+app.get('/', home)
+app.get('/about', about)
+app.get('/create', create)
+app.get('/details/:id', details)
+app.all('*',notFound)
+
+app.listen(3000, function () { console.log('listening on port 3000'); });
