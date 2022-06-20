93bed7c4d0d37645697f6fd8b6d9fa5a5f712156
DannyDoneva96 <danielasinmas1@gmail.com>
2022-06-20 10:43:14 +0300

    exam prep

--- Exam-Prep/index.js
+++ Exam-Prep/index.js
@@ -1,19 +1,32 @@
 //Loads the express module
 const express = require('express');
+
+
+const routes = require('./routes')
 //Loads the handlebars module
 const handlebars = require('express-handlebars');
+
 //Creates our express server
 const app = express();
 const port = 3000;
+
 //Sets our app to use the handlebars engine
 app.engine('hbs', handlebars.engine({
     extname: 'hbs'
 }))
 app.set('view engine', 'hbs');
+
 //Serves static files (we need it to import a css file)
 app.use(express.static('public'))
+
 // parse the body 
+
 app.use(express.urlencoded({ extended: false }))
+//Serves static files (we need it to import a css file)
+app.use(express.static('public'))
+
+app.use(routes)
+
 //Sets a basic route
 app.get('/', (req, res) => res.send('Hello World !'));
 
