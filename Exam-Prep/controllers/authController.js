be8455d94f5e82512a5c682b837e5b67f8806f8b
DannyDoneva96 <danielasinmas1@gmail.com>
2022-06-21 15:53:03 +0300

    some changes

--- Exam-Prep/controllers/authController.js
+++ Exam-Prep/controllers/authController.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
+
 const authService =require('../services/authService')
+
+const COOKIE_SESSION_NAME = 'user'
 
 router.get('/login',  (req, res)=>{
     res.render('auth/login')
@@ -10,19 +13,25 @@ router.get('/register',  (req, res)=>{
 })
 router.post('/login',  async(req, res)=>{
     const {username,password} = req.body
- const token =  await  authService.login(username, password)
+ const user =  await  authService.login(username, password)
+ const token = await authService.createToken(user)
+
+ res.cookie(COOKIE_SESSION_NAME,token);
+ res.redirect('/')
 })
 
 router.post('/register', async (req, res)=>{
-  const {username,password,repeatPassword,adress} = req.body;
+  const {password,repeatPassword,...userData} = req.body;
   if(password!==repeatPassword){
     return res.render('auth/register',{error: 'password is incorrect'})
  }
 
   try{
-    await authService.create({username,password,adress});
-    res.redirect('/login')
-  }catch(err){
+   const createdUser= await authService.create({password, ...userData});
+   const token = await authService.createToken(createdUser)
+   res.cookie(COOKIE_SESSION_NAME,token);
+    res.redirect('/')
+  }catch(error){
     return res.render('auth/register',{error: 'password is incorrect'})
   }
    res.end()
--- Exam-Prep/services/authService.js
+++ Exam-Prep/services/authService.js
@@ -3,28 +3,32 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 exports.create = (userData) => User.create(userData)
 const SECRET = '1234567890abcdef'
-exports.login = async(username,password) =>{
-   const user = await User.findOne({username})
-   if(!user){
-    throw{
-        message: ' This useer do not exist!'
+exports.login = async (username, password) => {
+    const user = await User.findOne({ username })
+    if (!user) {
+        throw {
+            message: ' This useer do not exist!'
+        }
     }
-   }
-  const isValid= bcrypt.compare(password, user.password)
-  if(!isValid){
-    throw{
-        message: ' Wrong Password'
+    const isValid = bcrypt.compare(password, user.password)
+    if (!isValid) {
+        throw {
+            message: ' Wrong Password'
+        }
     }
-  }
-  const payload = {_id: user._id,username: user.username, password: user.password}
-  const promise=new Promise((resolve, reject)=>{
-    jwt.sign(payload,SECRET,{expiresIn:'2d'},(err,decodedToken)=>{
-          if(err){
+    return user
+}
 
-            return reject(err)
-          }
-          resolve(decodedToken)
+exports.createToken=(user) => {
+    const payload = { _id: user._id, username: user.username, password: user.password }
+    const promise = new Promise((resolve, reject) => {
+        jwt.sign(payload, SECRET, { expiresIn: '2d' }, (err, decodedToken) => {
+            if (err) {
+
+                return reject(err)
+            }
+            resolve(decodedToken)
+        })
     })
-  })
-  return promise
+    return promise
 }
